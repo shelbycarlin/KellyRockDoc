fdbd6eb8d9b3764233c6d11ad927d28bdeb9500d
Shelby Carlin <shelbycarlin13@gmail.com>
2023-03-06 11:11:41 -0800

    File Path Set up and config

---
+++ kellyrockdoc/app/articles/page.tsx
@@ -0,0 +1,5 @@
+export default function Articles() {
+  return (
+  <div>Exotic Articles</div>
+  )
+}
---
+++ kellyrockdoc/app/presentations/page.tsx
@@ -0,0 +1,5 @@
+export default function Presentations() {
+  return (
+  <div>Presentations</div>
+  )
+}
